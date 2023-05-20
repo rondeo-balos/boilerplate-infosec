f8b9e58191c34a1cfb98c2da4e989a69c6f170cd
rondeo-balos <47505842+rondeo-balos@users.noreply.github.com>
2023-05-20 16:47:07 +0000

    Challenge Done

--- myApp.js
+++ myApp.js
@@ -6,14 +6,25 @@ const ninetyDaysInSeconds = 90*24*60*60;
 
 module.exports = app;
 const api = require('./server.js');
-app.use(helmet.hidePoweredBy());
+/*app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action:'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+//app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.contentSecurityPolicy({ directives: {defaultSrc:["'self'"],scriptSrc:["'self'",'trusted-cdn.com']} }));*/
+
 app.use(helmet.noCache()); //If you are releasing an update for your website, and you want the users to always download the newer version, you can (try to) disable caching on client’s browser.
+app.use( helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"]
+    }
+  }
+}) );
+
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
