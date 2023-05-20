2e2eeead70e6d96342f8ab1d52dbfc28ae0d498d
rondeo-balos <47505842+rondeo-balos@users.noreply.github.com>
2023-05-20 16:35:54 +0000

    Set a Content Security Policy with helmet.contentSecurityPolicy()

--- myApp.js
+++ myApp.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+
+const ninetyDaysInSeconds = 90*24*60*60;
 
 module.exports = app;
 const api = require('./server.js');
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action:'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache()); //If you are releasing an update for your website, and you want the users to always download the newer version, you can (try to) disable caching on client’s browser.
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
@@ -13,5 +24,3 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
-
-helmet.hidePoweredBy();
